489e58d7ca3d35d6f36140402313778a1d24fd1e
Brett Marshall <graphfx21@gmail.com>
2014-12-13 10:26:10 -0800

    updates node modules. added gulp-sass, gulp-autoprefixer. reduced core scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp'),
   gutil = require('gulp-util'),
-  webserver = require('gulp-webserver');
+  webserver = require('gulp-webserver'),
+  autoprefixer = require('gulp-autoprefixer'),
+  sass = require('gulp-sass');
 
 gulp.task('js', function() {
   gulp.src('builds/development/js/**/*')
@@ -10,13 +12,30 @@ gulp.task('html', function() {
   gulp.src('builds/development/*.html')
 });
 
+gulp.task('sass', function () {
+    gulp.src('builds/development/sass/**/*')
+        .pipe(sass())
+        .pipe(gulp.dest('builds/development/css'));
+});
+
 gulp.task('css', function() {
   gulp.src('builds/development/css/*.css')
+});
+
+gulp.task('autoprefixer', function () {
+    gulp.src('builds/development/css/*.css')
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('builds/development/css'));
 });
 
 gulp.task('watch', function() {
   gulp.watch('builds/development/js/**/*', ['js']);
   gulp.watch('builds/development/css/*.css', ['css']);
+  gulp.watch('builds/development/css/*.css', ['autoprefixer']);
+  gulp.watch('builds/development/sass/**/*', ['sass']);
   gulp.watch(['builds/development/*.html',
     'builds/development/views/*.html'], ['html']);
 });
@@ -25,8 +44,9 @@ gulp.task('webserver', function() {
   gulp.src('builds/development/')
     .pipe(webserver({
       livereload: true,
-      open: true
+      open: true,
+      host: "workpen.dev"
     }));
 });
 
-gulp.task('default', ['watch', 'html', 'js', 'css', 'webserver']);
+gulp.task('default', ['watch', 'html', 'js', 'sass', 'css', 'autoprefixer', 'webserver']);
